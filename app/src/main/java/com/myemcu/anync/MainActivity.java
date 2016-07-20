c1dd2aa5649cbd06f0b792e38fc4543a8c02c343
myemcu <1142497680@qq.com>
2016-07-20 23:49:45 +0800

    V3，在V2基础上，Go3传入参数6，且每秒更新显示。

--- app/src/main/java/com/myemcu/anync/MainActivity.java
+++ app/src/main/java/com/myemcu/anync/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btn_Go3(View v) {
-
+        new Job3Task().execute(6);
     }
 
     class Job1Task extends AsyncTask<Void, Void, Void> {
@@ -73,6 +73,45 @@ public class MainActivity extends AppCompatActivity {
 
             TextView info = (TextView) findViewById(R.id.txt);
             info.setText("DoneGo2");
+        }
+    }
+
+    class Job3Task extends AsyncTask <Integer, Integer, Void> {
+
+        private TextView info;
+
+        public Job3Task() { // 构造方法
+            info = (TextView) findViewById(R.id.txt);
+        }
+
+        @Override
+        protected Void doInBackground(Integer... params) {
+
+            for (int i=0; i<params[0]; i++) {
+                publishProgress(i); // 呼叫第111行的onProgressUpdate()秒更方法
+                try {
+                    Thread.sleep(1000);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            info.setText("DoneGo3");
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+            info.setText(String.valueOf(values[0]));
         }
     }
 }
